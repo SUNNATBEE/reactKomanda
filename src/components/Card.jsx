37346a7b64f9d694e72c6b96422adb2ab3b9fc2d
Orashi6 <Bekb0323@gmail.com>
2025-06-04 19:17:42 +0500

    component qoshdim yengi

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='bg-amber-700 p-4 rounded-lg shadow-lg text-white'>
+        Ustoz shuni manga berdiz,Man Azimjonman Ctrl+W+Shift
+    </div>
+  )
+}
+
+export default Card
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-amber-700'> Sunnatbek Header</div>
+    <div className='bg-amber-700'> Sunnatbek Header
+    <h1></h1>
+    </div>
+    
   )
 }
 
