5252664dc204de273c1b6cde12ad401aa1e6c721
SUNNATBEE <yusupovsunnatbek32@gmail.com>
2025-06-04 19:36:36 +0500

    Ogiziga

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import Header from './components/Header'
-import Something from './components/Something'
+import Hero from './components/Hero'
+import Main from "./components/Main"
+import Something from "./components/Something"
+import Card from "./components/Card"
+
 
 const App = () => {
   return (
     <div>
 
-
-      <Header />
-      <Hero/>
-
-      <Header/>
-
-      <Something />
-
-      <Card />
+<Header/>
+<Hero/>
+<Card/>
+<Main/>
+<Something/>
+      
 
 
     </div>
