a37149c4e2169ee577854b8ea700e6ce84376d52
Wert-bity77 <kabulovt770@gmail.com>
2025-06-04 19:24:48 +0500

    man qildim

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import Something from './components/Something'
 
 const App = () => {
   return (
     <div>
 
       <Header/>
+      <Something />
     </div>
   )
 }
